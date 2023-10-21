8ecb82a54719c6dd936c6cef1ed815b05ded5387
Dominik <dominik.zemlik.dev@gmail.com>
2023-10-21 16:13:14 +0200

    Add view for dispaly products list

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,14 +1,67 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using orders_asp_net_app.Models;
 
 namespace orders_asp_net_app.Controllers
 {
     public class ProductController : Controller
     {
+        private static IList<Product> ProductList = new List<Product>
+        {
+           new Product()
+           { 
+                Id = 1,
+                Name = "Smartphone",
+                Description = "Latest model smartphone with high-resolution camera",
+                Price = 599.99m,
+                Weight = 0.3m,
+                Quantity = 100,
+                Category = ProductCategory.ELECTRONICS
+           },
+           new Product() 
+           {
+                Id = 2,
+                Name = "Men's T-Shirt",
+                Description = "Comfortable cotton T-shirt for men",
+                Price = 19.99m,
+                Weight = 0.2m,
+                Quantity = 200,
+                Category = ProductCategory.FASHION
+           },
+           new Product()
+           {
+                Id = 3,
+                Name = "Vitamins",
+                Description = "Multivitamins for daily health",
+                Price = 9.99m,
+                Weight = 0.05m,
+                Quantity = 500,
+                Category = ProductCategory.HEALTH
+           },
+           new Product()
+           {
+                Id = 4,
+                Name = "Gaming Console",
+                Description = "Next-gen gaming console for immersive gaming experience",
+                Price = 499.99m,
+                Weight = 4.0m,
+                Quantity = 50,
+                Category = ProductCategory.ENTERTAINMENT
+           },
+           new Product() {
+                Id = 5,
+                Name = "Running Shoes",
+                Description = "High-performance running shoes for athletes",
+                Price = 79.99m,
+                Weight = 0.5m,
+                Quantity = 150,
+                Category = ProductCategory.SPORT
+           }
+        };
+
         // GET: ProductController
         public ActionResult Index()
         {
-            return View();
+            return View(ProductList);
         }
 
         // GET: ProductController/Details/5
