ff79710d7607cc7c677eba8035590929d1e7dfe5
Dominik <dominik.zemlik.dev@gmail.com>
2023-10-21 15:48:23 +0200

    Add Order and Product models

---
+++ Models/Order.cs
@@ -0,0 +1,25 @@
+﻿namespace orders_asp_net_app.Models
+{
+    public enum Payment
+    {
+        CASH,
+        CREDIT,
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;  
+
+        public Product[] Products { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public string ClientName { get; set; }  
+
+        public string ClientAdress { get; set; }    
+
+        public int ClientPhone { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,28 @@
+﻿namespace orders_asp_net_app.Models
+{
+    public enum ProductCategory
+    {
+        ELECTRONICS,
+        FASHION,
+        HEALTH,
+        ENTERTAINMENT,
+        SPORT
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+
+        public decimal Price { get; set; } 
+        
+        public decimal Weight { get; set; }
+
+        public int Quantity { get; set; } 
+
+        public ProductCategory Category { get; set; }
+    }
+}
